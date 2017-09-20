8cab7c702505ed71eac1781270ab7b2b0cde1865
Audiophilia <32020768+Audiophilia@users.noreply.github.com>
2017-09-20 09:57:34 -0700

    Add files via upload
    Updated to include MakeOutputReal after iFFT for the case that manipulation makes significant imaginary components.

--- iFFT_Engine_Template.m
+++ iFFT_Engine_Template.m
@@ -33,7 +33,7 @@ FFTSliceArray = BuildFFTSliceArray(inputSig, window, stepSize);
 
 
 %% Take iFFT and overlap add
-
-outputSig = iFFTOverlapAdd(FFTSliceArray);
+makeOutputReal = true;
+outputSig = iFFTOverlapAdd(FFTSliceArray, makeOutputReal);
 
 sound(outputSig,Fs);
